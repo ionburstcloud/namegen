b071c3e8130ca04b3b4054790f18bf48f7027eb0
Josh Fraser <josh_fraser@ionburst.io>
2022-04-01 19:19:18 +0100

    fix: sort initial version flag for cli

--- cli.go
+++ cli.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"gitlab.com/ionburst/namegen/namegen"
 )
 
+var appVersion string = "version"
+
 func main() {
+	var version bool = false
+	var v bool = false
+
+	flag.BoolVar(&version, "version", false, "Return namegen version")
+	flag.BoolVar(&v, "v", false, "Return namegen version")
+	flag.Parse()
+
+	if version || v {
+		fmt.Println(appVersion)
+		os.Exit(0)
+	}
+
 	fmt.Println(namegen.GetRandomName())
 }
